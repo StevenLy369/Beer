7b20ff53c1cdce2ba3aaa76bd7570c28c67c1556
Steven Ly <1stevenly92@gmail.com>
2020-07-03 15:52:37 -0700

    added beerdetail and going to work on getting detail to display

---
+++ src/component/BeerDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TicketDetails(props){
+    return(
+        <React.Fragment>
+            <h1>Ticket Detail</h1>
+            <hr/>
+        </React.Fragment>
+    )
+}
+
+
+export default TicketDetails;
