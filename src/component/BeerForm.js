5ab41a186617c1fc7db9ff1334cc34424fc17b82
Steven Ly <1stevenly92@gmail.com>
2020-07-03 13:27:48 -0700

    nothing happens when submitting

--- src/component/BeerForm.js
+++ src/component/BeerForm.js
@@ -1,13 +1,29 @@
 import  { v4 } from 'uuid'
 import React from 'react'
+import PropTypes from "prop-types"
 
 
-function BeerForm() {
+function BeerForm(props) {
+
+    function handleBeerSubmission(event){
+        event.preventDefault();
+        props.onNewBeer({
+            name: event.target.beerName.value,
+            brand: event.target.beerName.value,
+            price: event.target.beerPrice.value,
+            alcoholContent: event.target.alcoholContent.value,
+            pints: 124,
+            id: v4()
+        })
+    }
+
+
+
     return ( 
 
         <React.Fragment>
 
-        <form>
+        <form onSubmit={handleBeerSubmission}>
             <input type='text' name='beerName' placeholder='Enter Item Name'/>
             <input type='text' name='beerBrand' placeholder='Enter Beer Brand'/>
             <input type='number' name='beerPrice' placeholder='Enter Beer Cost'/>
@@ -19,10 +35,12 @@ function BeerForm() {
         </React.Fragment>
         
     )
+   
+    
 }
-
-
-
+BeerForm.prototype = {
+    onNewBeer: PropTypes.func
+}
 
 
 
