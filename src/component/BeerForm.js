2720e57870c137b7b52616806b7cf0decb06d8d1
Steven Ly <1stevenly92@gmail.com>
2020-07-03 09:05:42 -0700

    did beerform

---
+++ src/component/BeerForm.js
@@ -0,0 +1,29 @@
+import  { v4 } from 'uuid'
+import React from 'react'
+
+
+function BeerForm() {
+    return ( 
+
+        <React.Fragment>
+
+        <form>
+            <input type='text' name='beerName' placeholder='Enter Item Name'/>
+            <input type='text' name='beerBrand' placeholder='Enter Beer Brand'/>
+            <input type='number' name='beerPrice' placeholder='Enter Beer Cost'/>
+            <input type='number' name='alcoholContent' placeholder='Enter Alcohol Content'/>
+            <button type='submit'>Help</button>
+        </form>
+
+            
+        </React.Fragment>
+        
+    )
+}
+
+
+
+
+
+
+export default BeerForm;
